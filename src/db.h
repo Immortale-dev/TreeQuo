956030ba5e138482d352e46d910fa2d8bee4c881
Immortal <web.developer.ukraine@gmail.com>
2019-12-14 17:29:21 +0200

    initial commit

---
+++ src/db.h
@@ -0,0 +1,19 @@
+#ifndef DB_H
+#define DB_H
+
+#include "bplustree.hpp"
+#include "dbfs.hpp"
+#include "listcache.hpp"
+
+class DB{
+	
+	public:
+		DB();
+		~DB();
+		
+	private:
+		
+	
+}
+
+#endif // DB_H
