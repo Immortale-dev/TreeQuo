9ae10e454da086de17f98fa0cb192dcc7da3d293
Immortal <web.developer.ukraine@gmail.com>
2020-01-14 22:35:01 +0200

    multithreading tests added

--- test/src/tree.test.cpp
+++ test/src/tree.test.cpp
@@ -89,5 +89,128 @@ DESCRIBE("Test single thread", {
 
 
 DESCRIBE("Test multi threads", {
-	//
+	DESCRIBE("Initialize forest at tmp/t2", {
+		
+		DB forest;
+		
+		BEFORE_ALL({
+			forest.bloom("tmp/t2");
+		});
+		
+		AFTER_ALL({
+			forest.fold();
+			// Remove dirs?
+		});
+		
+		DESCRIBE("Add 100 trees in 10 threads", {
+			BEFORE_ALL({
+				vector<thread> trds;
+				for(int i=0;i<10;i++){
+					thread t([&forest](int i){
+						while(i<100){
+							forest.create_tree(TREE_TYPES::KEY_STRING, "test_string_tree_"+to_string(i));
+							i+=10;
+						}
+					},i);
+					trds.push_back(move(t));
+				}
+				for(int i=0;i<10;i++){
+					trds[i].join();
+				}
+			});
+			
+			IT("Trees should be created", {
+				int fc = dir_count("tmp/t2");
+				EXPECT(fc).toBeGreaterThanOrEqual(202);
+				INFO_PRINT("Dirs count: " + to_string(fc));
+			});
+			
+			DESCRIBE("Then remove 100 trees in 10 threads", {
+				BEFORE_ALL({
+					vector<thread> trds;
+					for(int i=0;i<10;i++){
+						thread t([&forest](int i){
+							while(i<100){
+								forest.delete_tree("test_string_tree_"+to_string(i));
+								i+=10;
+							}
+						},i);
+						trds.push_back(move(t));
+					}
+					for(int i=0;i<10;i++){
+						trds[i].join();
+					}
+				});
+				
+				IT("Trees should be deleted", {
+					int fc = dir_count("tmp/t2");
+					EXPECT(fc).toBeLessThanOrEqual(2);
+					INFO_PRINT("Dirs count: " + to_string(fc));
+				});
+			});
+		});
+		
+		
+		
+		DESCRIBE("Add 100 trees in random shuffle", {
+			vector<int> nums;
+			int cnt = 100;
+			
+			BEFORE_ALL({
+				for(int i=0;i<cnt;i++){
+					nums.push_back(i);
+				}
+				for(int i=0;i<cnt;i++){
+					swap(nums[i],nums[rand()%cnt]);
+				}
+				
+				vector<thread> trds;
+				for(int i=0;i<10;i++){
+					thread t([&cnt,&nums,&forest](int i){
+						while(i<cnt){
+							forest.create_tree(TREE_TYPES::KEY_STRING, "tree_"+to_string(nums[i]));
+							i+=10;
+						}
+					},i);
+					trds.push_back(move(t));
+				}
+				for(int i=0;i<10;i++){
+					trds[i].join();
+				}
+			});
+			
+			IT("Trees should be created", {
+				int fc = dir_count("tmp/t2");
+				EXPECT(fc).toBeGreaterThanOrEqual(cnt*2+2);
+				INFO_PRINT("Dirs count: " + to_string(fc));
+			});
+			
+			DESCRIBE("Then remove all trees in random shuffle in 10 threads", {
+				BEFORE_ALL({
+					for(int i=0;i<cnt;i++){
+						swap(nums[i],nums[rand()%cnt]);
+					}
+					vector<thread> trds;
+					for(int i=0;i<10;i++){
+						thread t([&cnt,&nums,&forest](int i){
+							while(i<cnt){
+								forest.delete_tree("test_string_tree_"+to_string(nums[i]));
+								i+=10;
+							}
+						},i);
+						trds.push_back(move(t));
+					}
+					for(int i=0;i<10;i++){
+						trds[i].join();
+					}
+				});
+				
+				IT("Trees should be deleted", {
+					int fc = dir_count("tmp/t2");
+					EXPECT(fc).toBeLessThanOrEqual(2);
+					INFO_PRINT("Dirs count: " + to_string(fc));
+				});
+			});
+		});
+	});
 });
