525736dba0810cab5c7015c27b54ab403ae18a13
Immortal <web.developer.ukraine@gmail.com>
2020-09-28 00:39:23 +0300

    tests update

--- src/variables.cpp
+++ src/variables.cpp
@@ -5,11 +5,11 @@ namespace forest{
 	const string LEAF_NULL = "-";
 
 	string ROOT_TREE = "_root";
-	int ROOT_FACTOR = 100;
-	int DEFAULT_FACTOR = 100;
-	int INTR_CACHE_LENGTH = 100;
-	int LEAF_CACHE_LENGTH = 100;
-	int TREE_CACHE_LENGTH = 100;
+	int ROOT_FACTOR = 3;
+	int DEFAULT_FACTOR = 3;
+	int INTR_CACHE_LENGTH = 3;
+	int LEAF_CACHE_LENGTH = 3;
+	int TREE_CACHE_LENGTH = 3;
 	int CACHE_BYTES = 512;
 	int CHUNK_SIZE = 1024;
 	int OPENED_FILES_LIMIT = 200;
